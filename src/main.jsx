64c7bde2710d62fe8572c31fa8000b64b0cc9f45
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-15 00:03:22 +0700

    Delete Default App file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
 import './index.css'
 
 import {
@@ -18,6 +17,5 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    {/* <App /> */}
   </React.StrictMode>,
 )
