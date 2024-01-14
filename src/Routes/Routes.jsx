59072b2063956caf69a952c9216eb5244b26d16a
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-15 00:07:26 +0700

    Added Routing system

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Obsnest Market By SadakatHossainSrabon</div>,
+    },
+  ]);
+
+export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import router from './Routes/Routes';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Obsnest Market By SadakatHossainSrabon</div>,
-  },
-]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
