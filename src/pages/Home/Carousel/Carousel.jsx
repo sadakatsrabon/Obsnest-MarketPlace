555acea74e6d179b67a29fcae217a1fa6eaa9c4e
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-16 21:57:36 +0700

    Implement Top reted Prodect Section on carousel

--- src/pages/Home/Carousel/Carousel.jsx
+++ src/pages/Home/Carousel/Carousel.jsx
@@ -2,8 +2,9 @@
 
 const Carousel = () => {
     return (
-        <div className="text-7xl font-bold">
-            Here will implement Carousel
+        <div className="md:flex flex-row-reverse justify-around my-10">
+            <h2 className="text-7xl font-bold">Here will implement Carousel</h2>
+            <div className="w-64 text-3xl font-bold text-emerald-500">Top Reted Products</div>
         </div>
     );
 };
--- src/pages/Home/LetextProduct/LetestProduct.jsx
+++ src/pages/Home/LetextProduct/LetestProduct.jsx
@@ -2,7 +2,7 @@
 const LetestProduct = () => {
     return (
         <div>
-            Here avilable 100 + letext products. Click to expand or slide to right or left.
+            <h2 className="text-2xl text-pink-500">Here avilable 100 + letext products. Click to expand or slide to right or left.</h2>
         </div>
     );
 };
