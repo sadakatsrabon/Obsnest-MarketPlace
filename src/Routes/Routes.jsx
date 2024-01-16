a911a75611713b9bbdc82c6b6467f54ff5e7dc26
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-16 11:29:27 +0700

    Implement Layout system

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomeMain from '../pages/Home/HomeMain/HomeMain';
+import MainLayout from '../Layout/MainLayout';
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <HomeMain></HomeMain>
-    },
-  ]);
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <HomeMain></HomeMain>
+      }
+    ]
+  },
+]);
 
 export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import {
   RouterProvider,
 } from "react-router-dom";
-import router from './Routes/Routes';
+import router from './Routes/Routes'; 
 
 
 
