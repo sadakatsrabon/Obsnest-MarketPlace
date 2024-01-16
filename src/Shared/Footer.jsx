36339acfe873aa1f0dad6b3020b9820caac46b27
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-16 22:13:32 +0700

    Added Footer

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -2,7 +2,28 @@
 const Footer = () => {
     return (
         <div>
-            <h2 className="text-4xl bg-red-300 text-red-800 font-extrabold text-center py-8">Footer</h2>
+            <footer className="footer p-10 bg-neutral text-neutral-content">
+                <nav>
+                    <header className="footer-title">Services</header>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav>
+                <nav>
+                    <header className="footer-title">Company</header>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <header className="footer-title">Legal</header>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+            </footer>
         </div>
     );
 };
