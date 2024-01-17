f5033368ab95f0256d88101e3b138739b35a6b25
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-17 21:26:25 +0700

    Added Carousel on Home page

--- src/pages/Home/Carousel/Carousel.jsx
+++ src/pages/Home/Carousel/Carousel.jsx
@@ -1,10 +1,40 @@
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCreative } from 'swiper/modules';
+import 'swiper/css';
+import './styles.css';
+import 'swiper/css/effect-creative';
 
 const Carousel = () => {
     return (
         <div className="md:flex flex-row-reverse justify-around my-10">
             <h2 className="text-7xl font-bold">Here will implement Carousel</h2>
             <div className="w-64 text-3xl font-bold text-emerald-500">Top Reted Products</div>
+            <Swiper
+                grabCursor={true}
+                effect={'creative'}
+                creativeEffect={{
+                    prev: {
+                        shadow: true,
+                        translate: [0, 0, -400],
+                    },
+                    next: {
+                        translate: ['100%', 0, 0],
+                    },
+                }}
+                modules={[EffectCreative]}
+                className="mySwiper"
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
         </div>
     );
 };
