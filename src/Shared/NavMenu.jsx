9da671a0d48e1a962b61a2592304fc536d788ed1
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-19 20:50:05 +0700

    update nav button

--- src/Shared/NavMenu.jsx
+++ src/Shared/NavMenu.jsx
@@ -10,9 +10,9 @@ const NavMenu = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
+                            <li><a>Order</a></li>
                             <li>
-                                <a>User Action</a>
+                                <a>Order Review</a>
                                 <ul className="p-2">
                                     <li><a>Login</a></li>
                                     <li><a>LogOut</a></li>
@@ -25,10 +25,10 @@ const NavMenu = () => {
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
+                        <li><a>Order</a></li>
                         <li>
                             <details>
-                                <summary>Parent</summary>
+                                <summary>Order review</summary>
                                 <ul className="p-2">
                                     <li><a>Submenu 1</a></li>
                                     <li><a>Submenu 2</a></li>
