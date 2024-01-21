98e9f04267be2172609531bf4a09e10fc224b2a1
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-21 16:52:57 +0700

    Added Section Title component

---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const SectionTitle = ({ heading, subHeadin }) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SectionTitle;
+
+// 
+// const SectionTitle = ({ heading, subHeading }) => {
+//     return (
+//         <div className="md:w-6/12 mx-auto my-8 pt-6">
+//             <p className="text-center">{subHeading}</p>
+//             <h3 className="text-3xl font-bold text-center uppercase border-b-4 pb-6 mb-2">{heading}</h3>
+//         </div>
+//     );
+// };
+
+// export default SectionTitle;
