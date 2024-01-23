0824af43c155c03bf6bd3ed4cae016e3b0db2d3f
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-23 23:34:22 +0700

    Delete Category cart

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,10 +1,8 @@
-// import CategoryCard from "./CategoryCard";
 
 const Category = () => {
     return (
         <div>
             <h2>Here will show our Obsnest category</h2>
-            {/* <CategoryCard></CategoryCard> */}
         </div>
     );
 };
