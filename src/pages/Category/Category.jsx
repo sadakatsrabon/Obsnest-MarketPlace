d02df559d50e992f8c3001e707300697c120d345
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-23 20:30:02 +0700

    Create Category page

---
+++ src/pages/Category/Category.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Category = () => {
+    return (
+        <div>
+            <h2>Here will show our Obsnest category</h2>
+        </div>
+    );
+};
+
+export default Category;
