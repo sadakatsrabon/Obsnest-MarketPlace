a248b13af190c36fbddfd54a2dec240dfbe0d081
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-24 21:20:11 +0700

    Filter data for specific category

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -4,7 +4,8 @@ const Category = () => {
     const [loadedData, setLoadedData] = useState([])
     console.log(loadedData);
 
-    const smart_collection = loadedData.filter(Category === 'smart_collection')
+    const smart_collection = loadedData.filter(item => item.category === 'smart_collection')
+    // console.log(smart_collection);
 
     useEffect(() => {
         fetch('productsData.json')
@@ -15,7 +16,7 @@ const Category = () => {
     }, [])
     return (
         <div>
-            <h2>{smart_collection}</h2>
+            <h2>{smart_collection.name}</h2>
             <h2 className="pt-20">Here will show our Obsnest category</h2>
         </div>
     );
