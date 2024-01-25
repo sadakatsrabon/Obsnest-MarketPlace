d9c93dd2c60aec1b7b8c28569debda76727e6584
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-25 20:09:06 +0700

    Resolve error for home Main

---
+++ src/pages/Home/HomeMain/HomeMain.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import Carousel from "../Carousel/Carousel";
+import SideSwiper from "../Carousel/SiceSwiper/SideSwiper";
+
+const HomeMain = () => {
+
+    // const [productsData, setData] = useState([])
+
+    // useEffect(() => {
+    //     fetch('productsData.json')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setData(data)
+    //             console.log(productsData)
+    //         })
+    // }, [productsData])
+    return (
+        <div className="pt-14" >
+            <div className="sm:flex mx-auto">
+                <div className="sm:w-[75%]">
+                    <Carousel></Carousel>
+                </div>
+                <SideSwiper></SideSwiper>
+            </div>
+            {/* Category */}
+            <SectionTitle
+                subHeadin={"Special Category By OBSNEST Market"}
+                heading={"Category"}
+            ></SectionTitle>
+            {/* <Category></Category> */}
+        </div>
+    );
+};
+
+export default HomeMain;
