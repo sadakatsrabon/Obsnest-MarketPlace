7d1ee411864bde6a162e2156c17b04e2de8b65f1
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-26 18:35:44 +0700

    Added Category Cart

--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const CategoryCart = () => {
     return (
-        <div>
-            <h2 className='text-4xl text-center'>Onsnest Category Cart</h2>
+        <div className="card w-96 bg-base-100 shadow-xl image-full">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
