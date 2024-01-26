b6323ee8e34cc747b9e11af7e3712b163777d09f
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-26 18:33:30 +0700

    Added Category cart text

--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CategoryCart = () => {
     return (
         <div>
-            
+            <h2 className='text-4xl text-center'>Onsnest Category Cart</h2>
         </div>
     );
 };
