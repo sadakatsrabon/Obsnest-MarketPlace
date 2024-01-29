c0c80e57a9d3b86e4a8490baf24e09b69fe643a1
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-29 16:41:57 +0700

    Added Cart czrosel

--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -1,13 +1,39 @@
 
 const CategoryCart = () => {
     return (
-        <div className="card w-96 bg-base-100 shadow-xl image-full">
-            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+        <div>
+            <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="rounded-box" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="rounded-box" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" className="rounded-box" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" className="rounded-box" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" className="rounded-box" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" className="rounded-box" />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" className="rounded-box" />
+                </div>
+            </div>
+            {/* Cart */}
+            <div className="card w-96 bg-base-100 shadow-xl image-full">
+                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
                 </div>
             </div>
         </div>
