f0824c29b7d39b13589d0645bda6f4c7a60eb35c
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-30 22:32:00 +0700

    Update One cart only for checking purpose

--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -3,8 +3,15 @@ const CategoryCart = () => {
     return (
         <div>
             <div className="carousel carousel-center p-4 space-x-4 rounded-box">
-                <div className="carousel-item">
-                    <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="rounded-box" />
+                <div className="carousel-item w-96 card bg-base-100 shadow-xl image-full">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
                 </div>
                 <div className="carousel-item">
                     <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="rounded-box" />
