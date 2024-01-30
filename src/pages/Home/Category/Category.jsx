03fc037dc6f8a1e54b5464add3ebbdddf227889b
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-30 17:50:25 +0700

    Loade data form josn

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import CategoryCart from "./CategoryCart";
+import { useEffect } from "react";
 
 
 const Category = () => {
+    const [loadedData, setLoadedData] = useState([]);
+    console.log(loadedData);
+    useEffect(() => {
+        fetch('productsData.json')
+            .then(res => res.json())
+            .then(data => {
+                setLoadedData(data);
+            })
+    }, [])
     return (
         <div>
             <CategoryCart></CategoryCart>
--- src/pages/Home/Category/CategoryMenu.jsx
+++ src/pages/Home/Category/CategoryMenu.jsx
@@ -1,18 +1,9 @@
 
-import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 
 const CategoryMenu = () => {
 
-    const [loadedData, setLoadedData] = useState([]);
-    useEffect(() => {
-        fetch('productsData.json')
-            .then(res => res.json())
-            .then(data => {
-                setLoadedData(data)
-            })
-    }, [])
-    console.log(loadedData)
+
     return (
         <div>
             <Helmet>
