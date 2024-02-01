be2dbaadef7f09ad648902b9d0711a10a697d2e5
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-02-01 21:42:48 +0700

    Update Name

--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -3,9 +3,8 @@ const CategoryCart = ({ key, items, title }) => {
     // console.log(items, title)
     return (
         <div>
-
             {
-                items.map(item => {
+                items.map((item) => {
                     <div
                         key={key}
                         className="carousel-item w-56 md:w-96 h-72 card bg-base-100 shadow-xl image-full"
@@ -17,7 +16,7 @@ const CategoryCart = ({ key, items, title }) => {
             <div className="carousel-item w-56 md:w-96 h-72 card bg-base-100 shadow-xl image-full">
                 <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">{title}!</h2>
+                    <h2 className="card-title">Name</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
