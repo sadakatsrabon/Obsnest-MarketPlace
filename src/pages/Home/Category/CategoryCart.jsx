d4f3bedf0ec61b6493bd2fee31ba647e31eefc67
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-02-01 21:35:42 +0700

    Added again card

--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -14,7 +14,7 @@ const CategoryCart = ({ key, items, title }) => {
                 })
             }
 
-            {/* <div className="carousel-item w-56 md:w-96 h-72 card bg-base-100 shadow-xl image-full">
+            <div className="carousel-item w-56 md:w-96 h-72 card bg-base-100 shadow-xl image-full">
                 <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{title}!</h2>
@@ -23,7 +23,7 @@ const CategoryCart = ({ key, items, title }) => {
                         <button className="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
-            </div> */}
+            </div>
 
 
         </div>
