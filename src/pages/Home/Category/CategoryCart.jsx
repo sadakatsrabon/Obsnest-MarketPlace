641e903c3c811fd87da32378c4625a92208a2138
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-02-01 21:48:44 +0700

    Return data form map

--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -5,18 +5,20 @@ const CategoryCart = ({ key, items, title }) => {
         <div>
             {
                 items.map((item) => {
-                    <div
-                        key={key}
-                        className="carousel-item w-56 md:w-96 h-72 card bg-base-100 shadow-xl image-full"
-                    ><h2 className="text-7xl">anything</h2></div>
-                    console.log(item.name)
-                })
+                    return (
+                        <div
+                            key={key}
+                            className="carousel-item w-56 md:w-96 h-72 card bg-base-100 shadow-xl image-full"
+                        ><h2 className="text-7xl">anything</h2></div>
+                    // console.log(item.name)
+                    )
+            })
             }
 
             <div className="carousel-item w-56 md:w-96 h-72 card bg-base-100 shadow-xl image-full">
                 <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">Name</h2>
+                    <h2 className="card-title">{title}!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
