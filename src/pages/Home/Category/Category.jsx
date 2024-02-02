f1bd7af5e64757b2e609b237545a7da310f53ac0
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-02-02 20:29:45 +0700

    Cahnge Data loading

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -5,29 +5,11 @@ import { useEffect } from "react";
 
 
 const Category = () => {
-    const [loadedData, setLoadedData] = useState([]);
-
-
-    const phone = loadedData.filter(item => item.category === 'phone')
-    console.log(phone)
-    // console.log(loadedData);
-
-
-    useEffect(() => {
-        fetch('productsData.json')
-            .then(res => res.json())
-            .then(data => {
-                setLoadedData(data);
-            })
-    }, [])
+    
 
     return (
         <div>
-            <CategoryCart
-                key={phone._id}
-                items={phone}
-                title="Phone"
-            ></CategoryCart>
+            <CategoryCart></CategoryCart>
         </div>
     );
 };
--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 
-const CategoryCart = ({ key, items, title }) => {
+const CategoryCart = () => {
+    const [loadedData, setLoadedData] = useState([]);
+
+
+    const phone = loadedData.filter(item => item.category === 'phone')
+    console.log(phone)
+    // console.log(loadedData);
+
+
+    useEffect(() => {
+        fetch('productsData.json')
+            .then(res => res.json())
+            .then(data => {
+                setLoadedData(data);
+            })
+    }, [])
     // console.log(items, title)
     return (
         <div>
-            {
+
+
+            {/* {
                 items.map((item) => {
                     console.log(item)
                     return (
@@ -19,12 +37,12 @@ const CategoryCart = ({ key, items, title }) => {
                         </div>
                     )
                 })
-            }
+            } */}
 
             <div className="carousel-item w-56 md:w-96 h-72 card bg-base-100 shadow-xl image-full">
                 <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">{title}!</h2>
+                    <h2 className="card-title">Category!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
