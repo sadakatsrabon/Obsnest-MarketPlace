c15b508a43047528e0c33bb4de6b8b362048987d
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-02-02 19:46:04 +0700

    Setup Dynamic Cart Data

--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -5,12 +5,18 @@ const CategoryCart = ({ key, items, title }) => {
         <div>
             {
                 items.map((item) => {
+                    console.log(item)
                     return (
-                        <div 
-                        key={key} 
-                        className="carousel-item w-56 md:w-96 h-72 card bg-base-100 shadow-xl image-full"
-                        ><h2 className="text-7xl">anything</h2></div>
-                        // console.log(item.name)
+                        <div key={key} className="carousel-item w-56 md:w-96 h-72 card bg-base-100 shadow-xl image-full">
+                            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{item.name}!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
                     )
                 })
             }
