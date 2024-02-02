dca91ef614e410aa3e972ea7683f974373f39a17
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-02-02 21:01:37 +0700

    Change loading system

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import CategoryCart from "./CategoryCart";
+import { useEffect } from "react";
 
 
 
 const Category = () => {
-    
+    const [loadedData, setLoadedData] = useState([]);
+
+    // console.log(loadedData);
+
+
+    useEffect(() => {
+        fetch('productsData.json')
+            .then(res => res.json())
+            .then(data => {
+                setLoadedData(data);
+            })
+    }, [])
 
     return (
         <div>
-            <CategoryCart></CategoryCart>
+            <CategoryCart
+                items={loadedData}
+            ></CategoryCart>
         </div>
     );
 };
--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -1,31 +1,13 @@
-import { useEffect, useState } from "react";
 
-const CategoryCart = () => {
-    const [loadedData, setLoadedData] = useState([]);
-
-
-    const phone = loadedData.filter(item => item.category === 'phone')
-    console.log(phone)
-    // console.log(loadedData);
-
-
-    useEffect(() => {
-        fetch('productsData.json')
-            .then(res => res.json())
-            .then(data => {
-                setLoadedData(data);
-            })
-    }, [])
-    // console.log(items, title)
+const CategoryCart = ({ loadedData }) => {
+    console.log(loadedData)
     return (
         <div>
-
-
             {/* {
-                items.map((item) => {
+                loadedData.map((item) => {
                     console.log(item)
                     return (
-                        <div key={key} className="carousel-item w-56 md:w-96 h-72 card bg-base-100 shadow-xl image-full">
+                        <div className="carousel-item w-56 md:w-96 h-72 card bg-base-100 shadow-xl image-full">
                             <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">{item.name}!</h2>
@@ -42,7 +24,7 @@ const CategoryCart = () => {
             <div className="carousel-item w-56 md:w-96 h-72 card bg-base-100 shadow-xl image-full">
                 <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">Category!</h2>
+                    <h2 className="card-title">{'title'}!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
