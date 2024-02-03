c63063f4b86bcddd30dec4dcb99bc5d4272a7b79
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-02-03 08:53:41 +0700

    Added Cart Category name

--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -12,22 +12,22 @@ const CategoryCart = ({ items }) => {
     const kitchen = items.filter(item => item.category === 'kitchen')
 
     return (
-        <div className='grid grid-cols-4 gap-4'>
-            <div className="carousel-item h-72 card bg-base-100 shadow-xl image-full">
+        <div className='md:grid grid-cols-2 gap-4'>
+            <div className="h-72 px-2 py-4 card bg-base-100 shadow-xl image-full">
                 <figure><img src={s_phone} alt="Phone" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">{'title'}!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title">{'Mobile Phones'}!</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At !</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
             </div>
             {/* Cart 2   */}
-            <div className="carousel-item h-72 card bg-base-100 shadow-xl image-full">
+            <div className="h-72 px-2 py-4 card bg-base-100 shadow-xl image-full">
                 <figure> <img src={fashion} alt="Fashion" /> </figure>
                 <div className="card-body">
-                    <h2 className="card-title">{'title'}!</h2>
+                    <h2 className="card-title">{'Fashoion and Buauti'}!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
