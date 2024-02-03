a41f42400905a2992e5d174ed069d21d1df7f502
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-02-03 11:39:52 +0700

    update button

--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -30,7 +30,7 @@ const CategoryCart = ({ items }) => {
                     <h2 className="card-title">{'Fashoion and Buauti'}!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-primary">Exprore</button>
                     </div>
                 </div>
             </div>
