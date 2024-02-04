91d72ca9a7fc845325e80a8893c855d97c94cd5e
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-02-04 23:14:53 +0700

    Solved z problem

--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -19,7 +19,7 @@ const CategoryCart = ({ items }) => {
         <div className='md:grid grid-cols-2 gap-4'>
             <div className="h-72 px-2 py-4 card bg-base-100 shadow-xl image-full">
                 <figure><img src={s_phone} alt="Phone" /></figure>
-                <div className="card-body">
+                <div className="">
                     <h2 className="card-title">{'Mobile Phones'}!</h2>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At !</p>
                     <div className="card-actions justify-end">
@@ -30,7 +30,7 @@ const CategoryCart = ({ items }) => {
             {/* Cart 2   */}
             <div className="h-72 px-2 py-4 card bg-base-100 shadow-xl image-full">
                 <figure> <img src={fashion} alt="Fashion" /> </figure>
-                <div className="card-body">
+                <div className="">
                     <h2 className="card-title">{'Fashoion and Buauti'}!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
@@ -41,7 +41,7 @@ const CategoryCart = ({ items }) => {
             {/* cart - 3 */}
             <div className="h-72 px-2 py-4 card bg-base-100 shadow-xl image-full">
                 <figure> <img src={electronicImg} alt="img" /> </figure>
-                <div className="card-body">
+                <div className="">
                     <h2 className="card-title">{'Electronics'}!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
@@ -52,7 +52,7 @@ const CategoryCart = ({ items }) => {
             {/* cart -4 */}
             <div className="h-72 px-2 py-4 card bg-base-100 shadow-xl image-full">
                 <figure> <img src={plasticImg} alt="Plastic Img" /> </figure>
-                <div className="card-body">
+                <div className="">
                     <h2 className="card-title">{'Plascic Products'}!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
@@ -63,7 +63,7 @@ const CategoryCart = ({ items }) => {
             {/* cart-5 */}
             <div className="h-72 px-2 py-4 card bg-base-100 shadow-xl image-full">
                 <figure> <img src={kitchenImg} alt="img" /> </figure>
-                <div className="card-body">
+                <div className="">
                     <h2 className="card-title">{'Kitchen Accessories'}!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
