6bb9d11b05534a1e8f3aff84f0596af39f24c866
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-02-27 20:46:25 +0700

    Remove extra data

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -4,20 +4,13 @@ import useData from "../../../components/Hooks/useData";
 
 
 const Category = () => {
-    // const [data, setdata] = useState([]);
-
-    // const phone = data.filter(item => item.category === 'phone')
-    // console.log(phone)
-
-    // console.log(data);
-
     const [data] = useData()
 
     return (
         <div>
-            <CategoryCart
-                items={data}
-            ></CategoryCart>
+                <CategoryCart
+                    items={data}
+                ></CategoryCart>
         </div>
     );
 };
--- src/pages/Home/Category/CategoryCart.jsx
+++ src/pages/Home/Category/CategoryCart.jsx
@@ -9,11 +9,11 @@ const CategoryCart = ({ items }) => {
 
     console.log(items)
 
-    const phone = items.filter(item => item.category === 'phone')
-    const electronic = items.filter(item => item.category === 'electronic')
-    const fashion_beauti = items.filter(item => item.category === 'fashion_beauti')
-    const plastic = items.filter(item => item.category === 'plastic')
-    const kitchen = items.filter(item => item.category === 'kitchen')
+    // const phone = items.filter(item => item.category === 'phone')
+    // const electronic = items.filter(item => item.category === 'electronic')
+    // const fashion_beauti = items.filter(item => item.category === 'fashion_beauti')
+    // const plastic = items.filter(item => item.category === 'plastic')
+    // const kitchen = items.filter(item => item.category === 'kitchen')
 
     return (
         <div className='md:grid grid-cols-2 gap-4'>
@@ -28,50 +28,6 @@ const CategoryCart = ({ items }) => {
                 </div>
             </div>
             {/* Cart 2   */}
-            <div className="h-72 px-2 py-4 card bg-base-100 shadow-xl image-full">
-                <figure> <img src={fashion} alt="Fashion" /> </figure>
-                <div className="card-body">
-                    <h2 className="card-title">{'Fashoion and Buauti'}!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Exprore</button>
-                    </div>
-                </div>
-            </div>
-            {/* cart - 3 */}
-            <div className="h-72 px-2 py-4 card bg-base-100 shadow-xl image-full">
-                <figure> <img src={electronicImg} alt="img" /> </figure>
-                <div className="card-body">
-                    <h2 className="card-title">{'Electronics'}!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Exprore</button>
-                    </div>
-                </div>
-            </div>
-            {/* cart -4 */}
-            <div className="h-72 px-2 py-4 card bg-base-100 shadow-xl image-full">
-                <figure> <img src={plasticImg} alt="Plastic Img" /> </figure>
-                <div className="card-body">
-                    <h2 className="card-title">{'Plascic Products'}!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Exprore</button>
-                    </div>
-                </div>
-            </div>
-            {/* cart-5 */}
-            <div className="h-72 px-2 py-4 card bg-base-100 shadow-xl image-full">
-                <figure> <img src={kitchenImg} alt="img" /> </figure>
-                <div className="card-body">
-                    <h2 className="card-title">{'Kitchen Accessories'}!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Exprore</button>
-                    </div>
-                </div>
-            </div>
-
         </div>
     );
 };
