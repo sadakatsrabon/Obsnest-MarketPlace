759653c493549adb1230a49e962117e615571a15
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-03-02 20:39:58 +0700

    update data showing

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -5,13 +5,17 @@ const Category = (params) => {
     const plastic = params.plastic
     const electronic = params.electronic
     const fashion_beauty = params.fashion_beauty
-    console.log(smart[0])
+    // console.log(smart)
     return (
         <div>
             <div className="text-center">
-                <h2 className="text-4xl font-semibold">{smart[0].name}</h2>
+                <h2 className="text-4xl font-semibold">
+                    {
+                        smart.map() = item => <div key={item._id}></div>
+                   }
+                </h2>
 
-                <h2 className="text-4xl font-semibold">{kitchen.category}</h2>
+                <h2 className="text-4xl font-semibold"></h2>
                 <h2 className="text-4xl font-semibold">Component for Plastic Category</h2>
                 <h2 className="text-4xl font-semibold">Component for Electronics Category</h2>
                 <h2 className="text-4xl font-semibold">Component for Fashion and beauti Category</h2>
