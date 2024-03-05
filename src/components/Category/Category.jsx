b0d3dc9e18fabed85b873d4c7be00ced18ca1a3f
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-03-05 21:08:04 +0700

    Added Cart for Category Cart

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -13,7 +13,13 @@ const Category = (params) => {
                     <h2 className="text-4xl font-semibold">{smart[0]?.category}</h2>
                     {
                         smart.map((item => <div key={item?._id}>
-                            <h2>{item.name}</h2>
+                            <div className="card w-96 bg-base-100 shadow-xl">
+                                <div className="card-body">
+                                    <h2 className="card-title">Shoes!</h2>
+                                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                                </div>
+                                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                            </div>
                         </div>))
                     }
                 </div>
