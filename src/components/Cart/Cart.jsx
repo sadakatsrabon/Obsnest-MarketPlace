4f1131dc5a495f25fd87a3b150756ab1f7bf0e91
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-03-07 20:15:19 +0700

    Pass Data to cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,23 +1,24 @@
 
 
-const Cart = (params) => {
-    // console.log(params,"Params")
+const Cart = ({ smart, kitchen, plastic, electronic, fashion_beauty }) => {
+    const cartData = [{ smart, kitchen, plastic, electronic, fashion_beauty }]
+    console.log()
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            {/* map */}
-            {/* {
-                params.map(item => console.log(item))
-            } */}
-            <figure className="px-10 pt-10">
-                <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
-            </figure>
-            <div className="card-body items-center text-center">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
+        <div>
+            {
+                cartData.map((item, index) => <div key={index} className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>)
+            }
         </div>
     );
 };
--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,65 +1,58 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css/pagination';
 import './styles.css';
-// import required modules
 import { Pagination } from 'swiper/modules';
 import Cart from '../Cart/Cart';
 
-// const Category = ({ smart, kitchen, plastic, electronic, fashion_beauty }) => {
 const Category = ({ smart, kitchen, plastic, electronic, fashion_beauty }) => {
+    // const [, setProductData] = useState([{ smart }, { kitchen }, { plastic }, { electronic }, { fashion_beauty }]);
+
     const productData = [{ smart }, { kitchen }, { plastic }, { electronic }, { fashion_beauty }]
-    // console.log(productData, "fromProductData")
     return (
         <div>
             {
-                productData?.map((item, index) => <div key={index} className="text-center">
-                    <div>
-                        <h2 className="text-4xl font-semibold">{productData[0]?.category}</h2>
-                        {/* Swiper Slide */}
-                        <Swiper
-                            slidesPerView={1}
-                            spaceBetween={10}
-                            pagination={{
-                                clickable: true,
-                            }}
-                            breakpoints={{
-                                '@0.00': {
-                                    slidesPerView: 1,
-                                    spaceBetween: 10,
-                                },
-                                '@0.75': {
-                                    slidesPerView: 2,
-                                    spaceBetween: 20,
-                                },
-                                '@1.00': {
-                                    slidesPerView: 3,
-                                    spaceBetween: 40,
-                                },
-                                '@1.50': {
-                                    slidesPerView: 4,
-                                    spaceBetween: 50,
-                                },
-                            }}
-                            modules={[Pagination]}
-                            className="mySwiper"
-                        >
-                            {
-                                smart.map((item =>
-                                    <SwiperSlide key={item.name}> <Cart smartItem={item}></Cart> </SwiperSlide>
-                                ))
-                            }
-                        </Swiper>
-                    </div>
-
+                productData?.map((productItem, index) => <div key={index}
+                    className="text-center">
+                    <h2 className="text-4xl font-semibold">{"productItem.category"}</h2>
+                    {/* Swiper Slide */}
+                    <Swiper
+                        slidesPerView={1}
+                        spaceBetween={10}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        breakpoints={{
+                            '@0.00': {
+                                slidesPerView: 1,
+                                spaceBetween: 10,
+                            },
+                            '@0.75': {
+                                slidesPerView: 2,
+                                spaceBetween: 20,
+                            },
+                            '@1.00': {
+                                slidesPerView: 3,
+                                spaceBetween: 40,
+                            },
+                            '@1.50': {
+                                slidesPerView: 4,
+                                spaceBetween: 50,
+                            },
+                        }}
+                        modules={[Pagination]}
+                        className="mySwiper"
+                    >
+                        {
+                            productData.map(((item, index) =>
+                                <SwiperSlide key={index}>
+                                    <Cart item={item}></Cart>
+                                </SwiperSlide>
+                            ))
+                        }
+                    </Swiper>
                 </div>)
             }
         </div>
-
-
-
-
-
-
     );
 };
 
