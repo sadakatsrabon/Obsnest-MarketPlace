62f8f12d9519c6ed903c0d44214918bec2db3667
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-03-07 21:38:07 +0700

    Chage component for cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,24 +1,23 @@
 
 
-const Cart = ({ smart, kitchen, plastic, electronic, fashion_beauty }) => {
-    const cartData = [{ smart, kitchen, plastic, electronic, fashion_beauty }]
-    console.log()
+const Cart = (params) => {
+    // console.log(params,"Params")
     return (
-        <div>
-            {
-                cartData.map((item, index) => <div key={index} className="card w-96 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>)
-            }
+        <div className="card w-96 bg-base-100 shadow-xl">
+            {/* map */}
+            {/* {
+                params.map(item => console.log(item))
+            } */}
+            <figure className="px-10 pt-10">
+                <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -8,6 +8,7 @@ const Category = ({ smart, kitchen, plastic, electronic, fashion_beauty }) => {
     // const [, setProductData] = useState([{ smart }, { kitchen }, { plastic }, { electronic }, { fashion_beauty }]);
 
     const productData = [{ smart }, { kitchen }, { plastic }, { electronic }, { fashion_beauty }]
+    //    console.log(productData)
     return (
         <div>
             {
@@ -45,7 +46,19 @@ const Category = ({ smart, kitchen, plastic, electronic, fashion_beauty }) => {
                         {
                             productData.map(((item, index) =>
                                 <SwiperSlide key={index}>
-                                    <Cart item={item}></Cart>
+
+                                    <div className="card w-96 bg-base-100 shadow-xl">
+                                        <figure className="px-10 pt-10">
+                                            <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                                        </figure>
+                                        <div className="card-body items-center text-center">
+                                            <h2 className="card-title">{item.name}</h2>
+                                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                                            <div className="card-actions">
+                                                <button className="btn btn-primary">Buy Now</button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </SwiperSlide>
                             ))
                         }
