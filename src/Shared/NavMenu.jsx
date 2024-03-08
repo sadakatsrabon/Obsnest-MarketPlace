089a40c69c4ae0dca2605bc6a6fadfe1160cd5b8
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-03-08 21:35:56 +0700

    Added SerarchAbar

--- src/Shared/NavMenu.jsx
+++ src/Shared/NavMenu.jsx
@@ -6,6 +6,7 @@ import logo from "../assets/logo.jpg"
 const NavMenu = () => {
     const navOptions =
         <>
+            {/* <input className="px-8 text-center" type="text" name="" placeholder="Type to search your product" id="" /> */}
             <li><Link to="/order/:salad"><button className='btn-outline text-white bg-black p-2 bg-opacity-40 uppercase font-semibold rounded-md'>Order Item</button></Link></li>
             <li><Link to='/dashboard/'><button className='btn-outline text-white bg-black p-2 bg-opacity-40 uppercase font-semibold rounded-md'>Dashboard</button></Link></li>
             <>
@@ -16,21 +17,30 @@ const NavMenu = () => {
     return (
         <div className="navbar h-1 fixed z-10 max-w-screen-2xl bg-opacity-30 md:bg-opacity-100 bg-black text-white">
             <div className="navbar-start">
+
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-slate-900 bg-opacity-80 rounded-box w-52">
+                    <Link className="" to="/"><img className='w-10 rounded-lg' src={logo} alt="logo" /></Link>
                         {navOptions}
+                        
                     </ul>
                 </div>
                 <Link to="/"><img className='w-10 rounded-lg' src={logo} alt="logo" /></Link>
+                <input className="px-8 py-2 ml-20 rounded-3xl text-black text-center" type="text" name="" placeholder="Type to search your product" id="" />
             </div>
+
             <div className="navbar-center hidden lg:flex">
+
                 <ul className="menu menu-horizontal px-1">
                     {navOptions}
+                    
                 </ul>
+
             </div>
+
         </div>
     );
 };
--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -18,63 +18,64 @@ const Category = ({ smart, kitchen, plastic, electronic, fashion_beauty }) => {
     return (
         <div>
             {
-                categories?.map((category, index) => <div key={index}
-                    className="text-center mt-10">
-                    <h2 className="text-4xl font-semibold">{category.title}</h2>
-                    {/* Swiper Slide */}
-                    <Swiper
-                        slidesPerView={1}
-                        spaceBetween={10}
-                        pagination={{
-                            clickable: true,
-                        }}
-                        breakpoints={{
-                            '@0.00': {
-                                slidesPerView: 1,
-                                spaceBetween: 10,
-                            },
-                            '@0.75': {
-                                slidesPerView: 2,
-                                spaceBetween: 20,
-                            },
-                            '@1.00': {
-                                slidesPerView: 3,
-                                spaceBetween: 40,
-                            },
-                            '@1.50': {
-                                slidesPerView: 4,
-                                spaceBetween: 50,
-                            },
-                        }}
-                        modules={[Pagination]}
-                        className="mySwiper"
-                    >
-                        {
-                            category.data.map(((item, index) =>
-                                <div className='' key={index}>
-                                    <SwiperSlide>
-                                        <div className="card w-96 bg-base-100 shadow-xl">
-                                            <figure className="px-10 pt-10">
-                                                <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
-                                            </figure>
-                                            <div className="card-body items-center text-center">
-                                                <h2 className="card-title">{item.name}</h2>
-                                                <p>{item.description}</p>
-                                                <p>${item.price}</p>
-                                                {/* Button */}
-                                                <div className="flex gap-2">
-                                                    <button className="btn border-none border-b-2 btn-primary">Buy Now</button>
-                                                    <button className="btn btn-outline bg-sky-300 text-black">Add To Card</button>
-                                                </div>
+                categories?.map((category, index) =>
+                    <div key={index}
+                        className="text-center mt-10">
+                        <h2 className="text-4xl font-semibold">{category.title}</h2>
+                        {/* Swiper Slide */}
+                        <Swiper
+                            slidesPerView={1}
+                            spaceBetween={10}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            breakpoints={{
+                                '@0.00': {
+                                    slidesPerView: 1,
+                                    spaceBetween: 10,
+                                },
+                                '@0.75': {
+                                    slidesPerView: 2,
+                                    spaceBetween: 20,
+                                },
+                                '@1.00': {
+                                    slidesPerView: 3,
+                                    spaceBetween: 40,
+                                },
+                                '@1.50': {
+                                    slidesPerView: 4,
+                                    spaceBetween: 50,
+                                },
+                            }}
+                            modules={[Pagination]}
+                            className="mySwiper"
+                        >
+                            {
+                                category.data.map(((item) =>
+                                    <div className='' key={item._id}>
+                                        <SwiperSlide>
+                                            <div className="card w-96 bg-base-100 shadow-xl">
+                                                <figure className="px-10 pt-10">
+                                                    <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                                                </figure>
+                                                <div className="card-body items-center text-center">
+                                                    <h2 className="card-title">{item.name}</h2>
+                                                    <p>{item.description}</p>
+                                                    <p>${item.price}</p>
+                                                    {/* Button */}
+                                                    <div className="flex gap-2">
+                                                        <button className="btn border-none border-b-2 btn-primary">Buy Now</button>
+                                                        <button className="btn btn-outline bg-sky-300 text-black">Add To Card</button>
+                                                    </div>
 
+                                                </div>
                                             </div>
-                                        </div>
-                                    </SwiperSlide>
-                                </div>
-                            ))
-                        }
-                    </Swiper>
-                </div>)
+                                        </SwiperSlide>
+                                    </div>
+                                ))
+                            }
+                        </Swiper>
+                    </div>)
             }
         </div>
     );
