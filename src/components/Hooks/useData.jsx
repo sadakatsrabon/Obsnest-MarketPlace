21ac0b2935377c884fd3ac22667857f7eb745eee
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-03-15 11:05:45 +0700

    update useData

--- src/components/Hooks/useData.jsx
+++ src/components/Hooks/useData.jsx
@@ -1,46 +1,21 @@
-// import { useState } from "react";
-// import { useEffect } from "react";
-
-// const useData = () => {
-//     const [data, setData] = useState([]);
-//     const [loading, setLoading] = useState(true);
-//     useEffect(() => {
-//         fetch('productsData.json')
-//             .then(res => res.json())
-//             .then(data => {
-//                 setData(data)
-//                 setLoading(false);
-//             })
-//     }, [])
-//     return [data, loading]
-// }
-// export default useData;
-
-
-
-
 import { useState } from "react";
 import { useEffect } from "react";
 
 const useData = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("productsData.json")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error);
-        setLoading(false); // Set loading to false even on error
-      });
-  }, []);
-
-  return [data, loading, error];
-};
-
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        fetch('productsData.json')
+            .then(res => res.json())
+            .then(data => {
+                setData(data)
+                setLoading(false);
+            })
+    }, [])
+    return [data, loading]
+}
 export default useData;
+
+
+
+
