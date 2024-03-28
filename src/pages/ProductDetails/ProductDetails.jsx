cfd4fc0a96ec6dec14795f28fda3545b7535fdf1
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-03-28 21:56:45 +0700

    Solved some error

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,14 +1,20 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import useData from "../../components/Hooks/useData";
 
 const ProductDetails = () => {
     const { _id } = useParams();
     // const paramsId = router._id;
-    console.log(_id )
+    // console.log(_id )
     const loadedProduct = useData();
 
+    useEffect(() => {
+        fetch(`http://localhost:5173/product/product-details/${_id}`)
+            .then(res => res.json())
+            .then(data => console.log(data, "S-npData"))
+    }, [])
 
-    console.log(loadedProduct)
+    // console.log(loadedProduct)
     // const smart = loadedProduct[0].filter(item => item._id === `${_id}`);
 
     // console.log(loadedProduct, "Sting");
