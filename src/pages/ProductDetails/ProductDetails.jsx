cda2aec0da7ee8a8dbe29b66e49dbf7b8be66815
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-03-29 21:18:33 +0700

    Update Code

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,19 +1,38 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useData from "../../components/Hooks/useData";
+import { useEffect, useState } from "react";
 
 const ProductDetails = () => {
+    const [selectedItem, setSelectedItem] = useState(null);
     const { _id } = useParams();
-    // const paramsId = router._id;
     const loadedProduct = useData();
-    console.log(loadedProduct)
-    
+
+    useEffect(() => {
+        if (loadedProduct && loadedProduct.length > 0) {
+            const selectedProduct = loadedProduct.find(item => item._id === _id);
+            setSelectedItem(selectedProduct);
+            // console.log(selectedItem);
+        }
+    }, [_id, loadedProduct]);
+
+
+    useEffect(() => {
+        // This will log the updated value of selectedItem
+        console.log(selectedItem);
+    }, [selectedItem]);
+
+
+
+
     // console.log(_id )
     // console.log(loadedProduct);
 
+
+
+
     return (
         <div className="pt-20">
-            <h2>This is the product details page.</h2>
+            <h2>This is the product details page id no:{_id} form {loadedProduct[0].length} data.</h2>
         </div>
     );
 };
