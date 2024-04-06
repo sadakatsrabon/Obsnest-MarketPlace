80ba408edcbec144280f7900bbe74cf3160bb1ed
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-04-06 21:48:59 +0700

    update some style of Product Details page

--- src/components/CartDetails/CartDetails.jsx
+++ src/components/CartDetails/CartDetails.jsx
@@ -13,22 +13,22 @@ const CartDetails = ({ item, isLoadig }) => {
     const { img, name, brandName, description, color, deliveryStatus, price, offerStatus } = item;
     return (
         <div>
-            <Link to={`/product/product-details/${item._id}`}>
-                <figure><img className=" hover:scale-110 duration-75" src={img} alt="Movie" /></figure>
-            </Link>
-            <div className="card-body">
-                <h2 className="card-title text-2xl">{name}</h2>
-                <p>By {brandName}</p>
-                <div className="divider"></div>
-                <p>{description}</p>
-                <p>Color: {color}</p>
-                <p>Delivery Charge: {deliveryStatus}</p>
-                <div className="flex">
-                    <p className="text-yellow-800 font-semibold">Price: {price}</p>
-                    <p>Offer Status: {offerStatus}</p>
-                </div>
+            <div className="md:flex">
+                <figure><img className="w-[700px] p-4 rounded-3xl" src={img} alt="Movie" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title text-2xl">{name}</h2>
+                    <p>By {brandName}</p>
+                    <div className="divider"></div>
+                    <p>{description}</p>
+                    <p>Color: {color}</p>
+                    <p>Delivery Charge: {deliveryStatus}</p>
+                    <div className="flex">
+                        <p className="text-yellow-800 font-semibold">Price: {price}</p>
+                        <p>Offer Status: {offerStatus}</p>
+                    </div>
 
-                <div className="card-actions justify-center pt-5 pr-6">
+                </div>
+                <div className="card-actions md:mt-20 mb-8 justify-center pt-5 pr-6">
                     <button className="btn bg-yellow-400 w-52 text-black">Add To Cart</button>
                 </div>
             </div>
