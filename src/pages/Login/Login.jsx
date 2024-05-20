80c45f2e2b6fbedb7a5acc75b6ce54572351d079
Sadakat Hossain <sadakat.sh1@gmail.com>
2024-05-20 21:55:38 +0700

    update login page desigb

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,8 +2,8 @@
 const Login = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
+            <div className="hero-content">
+                <div className="grid grid-cols-">
                     <h1 className="text-5xl font-bold">Login now!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
