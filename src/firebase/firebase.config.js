fb7e4388bb251f949f733313b16e1927b6c6e1cf
Sadakat Hossain <sadakat.sh1@gmail.com>
2024-05-22 09:10:47 +0700

    Update Firebase code

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAemRm3MmipTNIMlXE78uiH3-oGUf106_A",
+  authDomain: "obsnest-market.firebaseapp.com",
+  projectId: "obsnest-market",
+  storageBucket: "obsnest-market.appspot.com",
+  messagingSenderId: "102165943590",
+  appId: "1:102165943590:web:5ce4ebb08bef532656dde7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
