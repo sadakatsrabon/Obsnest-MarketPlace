cff2719175c125ced7beab73f106416d95e927e8
Sadakat Hossain <sadakat.sh1@gmail.com>
2024-05-22 21:52:19 +0700

    Added Create User Method

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,18 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createContext } from "react";
-import { getAuth } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+
     const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    };
+    const logIn = (email, password) => {
+        return signInWithEmailAndPassword(email, password);
+    };
+    const logOut = (auth) => {
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false)
+        })
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
+
     const authInfo = {
         user,
-        loading
+        loading,
+        createUser,
+        logIn,
+        logOut
     }
     return (
         <AuthContext.Provider value={authInfo}>
