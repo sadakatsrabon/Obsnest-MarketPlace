66759ba10c0c5545e6deddb7bd23ca8a3cd5fa7b
Sadakat Hossain <sadakat.sh1@gmail.com>
2024-05-22 09:33:08 +0700

    Added Env file and safe from upload on git

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  
-  
-  
-  
-  
-  
+  apiKey: import.meta.VITE_apiKey,
+  authDomain: import.meta.VITE_authDomain,
+  projectId: import.meta.VITE_projectId,
+  storageBucket: import.meta.VITE_storageBucket,
+  messagingSenderId: import.meta.VITE_messagingSenderId,
+  appId: import.meta.VITE_appId
 };
 
 // Initialize Firebase
