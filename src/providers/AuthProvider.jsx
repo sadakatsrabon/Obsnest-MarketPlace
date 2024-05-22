92e4e7b551ff6a1495273266bcdd8449df7b6732
Sadakat Hossain <sadakat.sh1@gmail.com>
2024-05-22 10:25:21 +0700

    complete code for auth provider

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import { createContext } from "react";
+import { getAuth } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const authInfo = {
+        user,
+        loading
+    }
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={authInfo}>
             {children}
         </AuthContext.Provider>
     );
