1afd9da103a747c970721d2dad339eaafa607674
Sadakat Hossain <sadakat.sh1@gmail.com>
2024-05-31 15:50:47 +0700

    Added SignUp or login alart

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const SignUp = () => {
 
@@ -18,6 +19,29 @@ const SignUp = () => {
             .then(result => {
                 const createdUser = result.user;
                 console.log(createdUser);
+
+                let timerInterval;
+                Swal.fire({
+                    title: "Auto close alert!",
+                    html: "I will close in <b></b> milliseconds.",
+                    timer: 2000,
+                    timerProgressBar: true,
+                    didOpen: () => {
+                        Swal.showLoading();
+                        const timer = Swal.getPopup().querySelector("b");
+                        timerInterval = setInterval(() => {
+                            timer.textContent = `${Swal.getTimerLeft()}`;
+                        }, 100);
+                    },
+                    willClose: () => {
+                        clearInterval(timerInterval);
+                    }
+                }).then((result) => {
+                    /* Read more about handling dismissals below */
+                    if (result.dismiss === Swal.DismissReason.timer) {
+                        console.log("I was closed by the timer");
+                    }
+                });
             })
     }
 
