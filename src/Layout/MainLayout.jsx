865c5a039958450fe26286e00c387c7e12d7c431
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-07-04 10:31:55 +0700

    Added Universal sidebar

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,14 +1,20 @@
 import { Outlet } from "react-router-dom";
 import NavMenu from "../Shared/NavMenu";
 import Footer from "../Shared/Footer";
+import Sidebar from "../components/Sidebar/Sidebar";
 
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="">
+            <div>
+                <Sidebar></Sidebar>
+            </div>
+            <div>
             <NavMenu></NavMenu>
             <Outlet></Outlet>
             <Footer></Footer>
+            </div>
         </div>
     );
 };
