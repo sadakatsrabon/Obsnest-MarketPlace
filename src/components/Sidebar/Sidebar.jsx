0a19bba451d7f59557e1dea7340eff76282e5730
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-07-06 12:21:04 +0700

    Set sidebar position

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -13,8 +13,16 @@ const Sidebar = () => {
                 <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
                 <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
                     {/* Content here */}
-                    <div className="">
-                        <li><a>Sidebar Item 1</a></li>
+                    <div className="pt-20">
+                        <li><a>Smart</a></li>
+                        <li><a>Kitchen</a></li>
+                        <li><a>Plastic</a></li>
+                        <li><a>Electronic</a></li>
+                        <li><a>Fashion and beauty</a></li>
+                        <div className="divider"></div>
+                        <li><a>Sidebar Item 2</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                        <li><a>Sidebar Item 2</a></li>
                         <li><a>Sidebar Item 2</a></li>
                     </div>
                 </ul>
