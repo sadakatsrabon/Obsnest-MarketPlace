61e6f104ce25d516b87e08a4b6d02869f35d839d
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-07-11 13:15:28 +0700

    remove unexpected styles

--- src/components/Category/CartSwiper/CartSwiper.jsx
+++ src/components/Category/CartSwiper/CartSwiper.jsx
@@ -7,57 +7,59 @@ import { Link } from 'react-router-dom';
 
 const CartSwiper = ({ category }) => {
     return (
-        <Swiper
-            slidesPerView={1}
-            spaceBetween={10}
-            pagination={{
-                clickable: true,
-            }}
-            breakpoints={{
-                '@0.00': {
-                    slidesPerView: 1,
-                    spaceBetween: 10,
-                },
-                '@0.75': {
-                    slidesPerView: 2,
-                    spaceBetween: 20,
-                },
-                '@1.00': {
-                    slidesPerView: 3,
-                    spaceBetween: 40,
-                },
-                '@1.50': {
-                    slidesPerView: 4,
-                    spaceBetween: 50,
-                },
-            }}
-            modules={[Pagination]}
-            className="mySwiper"
-        >
-            {
-                category.data.map(((item) =>
-                    <div className='' key={item._id}>
-                        <SwiperSlide>
-                            <Link to={`product/product-details/${item._id}`} className="card w-96 bg-base-100 shadow-xl">
-                                <figure className="px-10 pt-10 hover:scale-110 duration-75 ">
-                                    <img src={item.img} alt="Shoes" className="rounded-xl" />
-                                </figure>
-                                <div className="card-body items-center text-center">
-                                    <h2 className="card-title">{item.name}</h2>
-                                    <p>{item.description}</p>
-                                    <div className='divider w-1/2 mx-auto my-0'></div>
-                                    <p>${item.price}</p>
-                                    <div className='gap-2'>
-                                        <p className='w-full px-4 border-2 border-orange-600 rounded-lg text-orange-600'>DeliveryC. {item.deliveryStatus}</p>
-                                        <p className='w-full px-4 border-2 bg-sky-500 text-white rounded-lg'>{item.offerStatus}$ Discount</p>
+        <div className=''>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={10}
+                pagination={{
+                    clickable: true,
+                }}
+                breakpoints={{
+                    '@0.00': {
+                        slidesPerView: 1,
+                        spaceBetween: 10,
+                    },
+                    '@0.75': {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                    '@1.00': {
+                        slidesPerView: 3,
+                        spaceBetween: 40,
+                    },
+                    '@1.50': {
+                        slidesPerView: 4,
+                        spaceBetween: 50,
+                    },
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                {
+                    category.data.map(((item) =>
+                        <div className='' key={item._id}>
+                            <SwiperSlide>
+                                <Link to={`product/product-details/${item._id}`} className="card w-96 bg-base-100 shadow-xl">
+                                    <figure className="px-10 pt-10 hover:scale-110 duration-75 ">
+                                        <img src={item.img} alt="Shoes" className="rounded-xl" />
+                                    </figure>
+                                    <div className="card-body items-center text-center">
+                                        <h2 className="card-title">{item.name}</h2>
+                                        <p>{item.description}</p>
+                                        <div className='divider w-1/2 mx-auto my-0'></div>
+                                        <p>${item.price}</p>
+                                        <div className='gap-2'>
+                                            <p className='w-full px-4 border-2 border-orange-600 rounded-lg text-orange-600'>DeliveryC. {item.deliveryStatus}</p>
+                                            <p className='w-full px-4 border-2 bg-sky-500 text-white rounded-lg'>{item.offerStatus}$ Discount</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </Link>
-                        </SwiperSlide>
-                    </div>
-                ))
-            }
-        </Swiper>
+                                </Link>
+                            </SwiperSlide>
+                        </div>
+                    ))
+                }
+            </Swiper>
+        </div>
     );
 };
 
--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -30,9 +30,11 @@ const Category = ({ smart, kitchen, plastic, electronic, fashion_beauty }) => {
                             className="text-center mt-10 border-b-4">
                             <h2 className="text-4xl font-semibold">{category.title}</h2>
 
-                            <CartSwiper
-                                category={category}
-                            ></CartSwiper>
+                            <div className="bg-sky-100 p-2">
+                                <CartSwiper
+                                    category={category}
+                                ></CartSwiper>
+                            </div>
 
                             <div className='text-end'>
                                 <Link to={`product/${category.title}`} className='btn btn-outline rounded-full text-3xl text-black'>{">"}</Link>
