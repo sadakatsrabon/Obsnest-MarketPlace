f23ee1ad17a83f0409accac500e36b3efd10cfbf
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-07-13 14:00:46 +0700

    Setup Login page style

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -60,7 +60,7 @@ const Login = () => {
 
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex flex-row-reverse">
+            <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="grid grid-cols-">
                     <h1 className="text-5xl font-bold">Login now!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
