7eaea3d73225f979e3b474fa64c5faa2b101542d
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-07-30 21:07:22 +0700

    Added reduce function to showing a total consumption for price

--- src/pages/DashBoard/dashboardPage/MyCart.jsx
+++ src/pages/DashBoard/dashboardPage/MyCart.jsx
@@ -2,9 +2,12 @@ import useCart from "../../../components/hooks/useCart";
 
 const MyCart = () => {
     const [cart] = useCart();
+    // Calculate Total Price Using Reduce Function. 
+    const accumulator = cart.reduce((sum, item) => item.price + sum, 0);
     return (
         <div>
             <h2>You Have Added {cart.length} Obsnest Item For Obsnesest Delivery</h2>
+            <h2>You Have To Pay {accumulator} $</h2>
         </div>
     );
 };
